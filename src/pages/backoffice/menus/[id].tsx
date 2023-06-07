3c5a2cff272140b5b5ccc45dd5f5b4ac8eddcc91
waihinsoe <waihinsoe332004@gmail.com>
2023-06-07 08:23:58 +0630

    feat: update menu and menuCategories

--- src/pages/backoffice/menus/[id].tsx
+++ src/pages/backoffice/menus/[id].tsx
@@ -1,44 +1,69 @@
 import { useContext, useEffect, useState } from "react";
 import { Autocomplete, Box, Button, TextField } from "@mui/material";
-import { Menu } from "../../../typings/types";
+// import { Menu } from "../../../typings/types";
+import type {
+  menus as Menu,
+  menu_categories as MenuCategory,
+} from "@prisma/client";
 import { config } from "../../../config/config";
 import Layout from "../../../components/Layout";
 import { BackOfficeContext } from "@/contexts/BackOfficeContext";
 import { useRouter } from "next/router";
+import { getSelectedLocationId } from "@/utils";
 
 const MenuDetail = () => {
   const router = useRouter();
   const menuId = router.query.id as string;
-  const { menus, menuCategories } = useContext(BackOfficeContext);
+  const { menus, menuCategories, menusMenuCategoriesLocations, fetchData } =
+    useContext(BackOfficeContext);
+  const selectedLocationId = getSelectedLocationId() as string;
+  const menuCategoryIds = menusMenuCategoriesLocations
+    .filter((item) => item.menus_id == parseInt(menuId, 10))
+    .map((item) => item.menu_categories_id);
 
   let menu: Menu | undefined;
   if (menuId) {
     menu = menus.find((menu) => menu.id === parseInt(menuId, 10));
   }
+  const selectedMenuCategories: MenuCategory[] = menuCategories.filter(
+    (menuCategory) => menuCategoryIds.includes(menuCategory.id)
+  );
+  const [selected, setSelected] = useState(selectedMenuCategories);
+
   const [newMenu, setNewMenu] = useState({
-    name: "",
-    price: 0,
+    id: menu?.id,
+    name: menu?.name,
+    price: menu?.price,
+    menuCategoryIds,
+    locationId: selectedLocationId,
   });
 
   useEffect(() => {
     if (menu) {
-      setNewMenu({ name: menu?.name, price: menu?.price });
+      setNewMenu({
+        id: menu.id,
+        name: menu.name,
+        price: menu.price,
+        menuCategoryIds,
+        locationId: selectedLocationId,
+      });
+      setSelected(selectedMenuCategories);
     }
   }, [menu]);
 
   const updateMenu = async () => {
-    const response = await fetch(
-      `${config.backOfficeApiBaseUrl}/menus/${menu?.id}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMenu),
-      }
-    );
-    console.log(await response.json());
+    const response = await fetch(`${config.backOfficeApiBaseUrl}/menus`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMenu),
+    });
+    if (response.ok) {
+      fetchData();
+    }
   };
+  if (!menu) return;
   return (
     <Layout>
       <Box>
@@ -75,9 +100,16 @@ const MenuDetail = () => {
             <Autocomplete
               multiple
               limitTags={2}
-              id="addonCategories"
+              id="menuCategories"
+              value={selected}
               options={menuCategories}
+              isOptionEqualToValue={(options, value) => options.id == value.id}
               getOptionLabel={(option) => option.name}
+              onChange={(evt, values) => {
+                const menuCategoryIds = values.map((item) => item.id);
+                setNewMenu({ ...newMenu, menuCategoryIds });
+                setSelected(values);
+              }}
               renderInput={(params) => (
                 <TextField
                   {...params}
