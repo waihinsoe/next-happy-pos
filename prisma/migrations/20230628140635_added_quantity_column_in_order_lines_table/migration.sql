624711701babc88c651fa9cee2c0a08208c23406
waihinsoe <waihinsoe332004@gmail.com>
2023-07-02 22:28:46 +0630

    feat: rework order

---
+++ prisma/migrations/20230628140635_added_quantity_column_in_order_lines_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "orderLines" ADD COLUMN     "quantity" INTEGER NOT NULL DEFAULT 1;
--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,6 +1,6 @@
 import type { menus as Menu, addons as Addon } from "@prisma/client";
 
-export interface OrderLine {
+export interface CartItem {
   menu: Menu;
   addons: Addon[];
   quantity: number;
